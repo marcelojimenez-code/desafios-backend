a89d2f52c6d3a5331773c361b79599f7fbc65c1d
Marcelo Jimenez C <marcelojimenezcubillos@gmail.com>
2024-05-30 23:37:59 -0400

    Desafios

--- desafio5/src/middleware/auth.js
+++ desafio5/src/middleware/auth.js
@@ -15,14 +15,20 @@ export const isNotAuthenticated = (req, res, next) => {
     }
 };
 
-export const verificarAdmin = (req, res, next) => {
-      // Suponiendo que el rol del usuario está almacenado en req.session.usuario.rol
-      if (req.session && req.session.usuario && req.session.usuario.rol === 'admin') {
-        // Si el usuario es administrador, redirigir al listado de usuarios
-        return res.redirect('/listado');
-      } else {
-        // Si el usuario no es administrador, redirigir a la página de productos
-        return res.redirect('/profile');
-      }
+export const isLoggedIn = (req, res, next) => {
+    if (!req.session.user) {
+        return next();
+    }
+    else {
+        if (req.session.usuario.role === 'admin') {
+            // Si el usuario es administrador, redirigir al listado de usuarios
+            return res.redirect('/listado');
+        } else {
+            // Si el usuario no es administrador, redirigir al profile
+            return res.redirect('/profile');
+        }
+
+    }    
+        
     
 };
